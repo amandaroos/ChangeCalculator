0dfccc415f29c0f65ccf01c871d4a9416782d49a
Amanda Farrell <bluepandadev@gmail.com>
2017-09-27 23:52:18 -0600

    Initial commit

---
+++ app/src/main/java/com/amandfarrell/www/changecalculator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.amandfarrell.www.changecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private double mTotal;
+    private double mCash;
+    private double mChangeDue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
